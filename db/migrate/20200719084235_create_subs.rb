5e42dc8b39cc41ce7bc716e1eadc88f7e4835235
shibahayato <shibahayaton@gmail.com>
2020-07-24 16:04:47 +0900

    change migration files

--- db/migrate/20200719084235_create_subs.rb
+++ db/migrate/20200719084235_create_subs.rb
@@ -2,7 +2,7 @@ class CreateSubs < ActiveRecord::Migration[5.2]
   def change
     create_table :subs do |t|
       t.string :name
-      t.string :type
+      t.string :kind
 
       t.timestamps
     end
--- db/migrate/20200720082434_add_subs_id_to_days.rb
+++
@@ -1,5 +0,0 @@
-class AddSubsIdToDays < ActiveRecord::Migration[5.2]
-  def change
-    add_column :days, :subs_id, :integer
-  end
-end
